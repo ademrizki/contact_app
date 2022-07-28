cc5bba89ea0ae398cbbd2c44e22e69f35586bdc8
ademrizki <ade.rizky@amartha.com>
2022-07-28 17:45:21 +0700

    Initial commit

---
+++ lib/src/view/screen/create_contact_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CreateContactScreen extends StatelessWidget {
+  const CreateContactScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create Contact'),
+      ),
+    );
+  }
+}
